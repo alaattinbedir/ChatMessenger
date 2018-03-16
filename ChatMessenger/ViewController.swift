a4c5447ae272971660427d9a9e975872944f255d
Alaattin Bedir <alaattinbedir@hotmail.com>
2018-03-16 11:53:12 +0300

    Initial Commit

---
+++ ChatMessenger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatMessenger
+//
+//  Created by Alaattin Bedir on 16.03.2018.
+//  Copyright © 2018 Magiclamp Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
